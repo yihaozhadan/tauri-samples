92f7afd9da7cbd8fd7fa71f48f111cb222cbec6a
hui@Huis-MacBook-Air <yihaozhadan@gmail.com>
2024-11-16 23:39:42 -0600

    Implement calculator sample

---
+++ calculator/src/main.ts
@@ -0,0 +1,11 @@
+import './app.css'
+import App from './App.svelte'
+import { mount } from 'svelte'
+
+const target = document.getElementById('app')
+
+if (target) {
+  mount(App, {
+    target: target,
+  })
+}
